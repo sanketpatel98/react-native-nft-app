74b59adfbb37a546dc62808801dd52dce65068c6
Sanket Patel <patelsanketr98@gmail.com>
2022-08-19 16:32:53 -0400

    first commit

---
+++ components/index.js
@@ -0,0 +1,8 @@
+import HomeHeader from "./HomeHeader";
+import FocusedStatusBar from "./FocusedStatusBar";
+import NFTCard from "./NFTCard";
+import { CircleButton, RectButton } from "./CircleButton";
+
+export {
+    HomeHeader,NFTCard,FocusedStatusBar
+}
